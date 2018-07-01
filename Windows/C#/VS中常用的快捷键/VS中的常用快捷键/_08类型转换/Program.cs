550ebf025339d7aaaa229bc9bb6b5c23ed0ff535
空白 <m13619199464@163.com>
2018-07-01 14:24:41 +0800

    添加了C#中的面向对象程序设计

---
+++ Windows/C#/VS中常用的快捷键/VS中的常用快捷键/_08类型转换/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08类型转换
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用Convert进行转换,成功了好说,失败了会抛异常
+            //int numberOne = Convert.ToInt32("1230");
+
+            //Convert在使用中会调用Parse
+            //直接使用Parse会比Convert效率更高
+            //二者效果是相同的
+
+            //int number = int.Parse("123abc");
+            //Console.WriteLine(number);
+
+            int number = 0;
+            //参数 返回值
+            bool b = int.TryParse("123", out number);
+            Console.WriteLine(b);
+            Console.WriteLine(number);
+            //方法 或者 函数?
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Windows/C#/VS中常用的快捷键/VS中的常用快捷键/乘法口诀表/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 乘法口诀表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 九九乘法表
+            //for (int i = 1; i <= 9; i++)
+            //{
+            //    for (int j = 1; j <= 9; j++)
+            //    {
+            //        Console.Write("{0}*{1}={2}\t", i, j, i * j);    //可以加入\n转义字符换行
+            //    }
+            //    Console.WriteLine();        //什么都不写表示换行
+            //}
+            //Console.ReadKey();
+            #endregion
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}*{1}={2}\t", i, j, i * j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
