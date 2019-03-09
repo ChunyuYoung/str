909c16ca2afc54c25b3af23dd52e91ba8bde3e43
空白 <m13619199464@163.com>
2019-03-09 15:38:58 +0800

    Update the name

--- Code/Java/003-DemoLongSubString/src/DemoLongSubStringMain.java
+++
@@ -1,9 +0,0 @@
-public class DemoLongSubStringMain {
-    public static void main(String args[]){
-        DemoLongSubString str = new DemoLongSubString();
-        System.out.println("shitianrui:"+str.GetSubStringLength("shitianrui"));
-        System.out.println("daqingwa:"+str.GetSubStringLength("daqingwa"));
-        System.out.println("chixiang:"+str.GetSubStringLength("chixiang"));
-        System.out.println("SssTttRrr:"+str.GetSubStringLength("SssTttRrr"));       //区分大小写
-    }
-}
