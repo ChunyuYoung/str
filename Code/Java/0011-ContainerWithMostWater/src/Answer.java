f1ba6fd713c5cf63084bf79c50c854c54db082de
空白 <m13619199464@163.com>
2019-03-09 21:28:33 +0800

    UpdateCode

--- Code/Java/0011-ContainerWithMostWater/src/Answer.java
+++ Code/Java/0011-ContainerWithMostWater/src/Answer.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Answer {
+    public int maximumCapacity(int height[]){
+        int maxCapacity = 0;    //最大容量
+
+        //1. 首先遍历数组,从第一个元素下标开始
+        for (int i = 0;i<height.length;i++){
+            //2. 遍历数组,从 i + 1 开始
+            for (int j = i+1;j<height.length;j++){
+                //取最大容量
+                maxCapacity = Math.max(maxCapacity,Math.min(height[i],height[j])*(j-i));
+//                maxCapacity = Math.max(maxCapacity,Math.min(height[i],height[j]));
+            }
+        }
+        return maxCapacity;
+    }
 }
--- Code/Java/0011-ContainerWithMostWater/src/Main.java
+++ Code/Java/0011-ContainerWithMostWater/src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String args[]){
+        Answer answer = new Answer();
+        int a[] = {10,20,30,40};
+        int b[] = {10};
+        System.out.println("a{10,20,30,40}最大容量:"+"\t\t\t"+answer.maximumCapacity(a));
+        System.out.println("b{10}最大容量:"+"\t\t\t"+answer.maximumCapacity(b));
+    }
 }
