58a8d8f1955a3ee2cc613a19525a22f91d1ebd28
StringOD <m2719859320@gmail.com>
2019-03-11 09:09:23 +0800

    UpdateJavaCode

--- Code/Java/0014-LongestCommonPrefix/src/Main.java
+++ Code/Java/0014-LongestCommonPrefix/src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
 
 public class Main {
+	public static void main(String args[]){
+		Answer ans = new Answer();
+		String s1[] = {"str","sstr","ssstr"}; 
+		String s2[] = {"shitianrui","str"};
+		String s3[] = {"flow","flo","fl"};
+		String s4[] = {"sssttr","str","s13"};
+		String s5[] = {};
+		String s6[] = {"str"};
+		System.out.println(ans.GetIntegerSubString(s1));
+		System.out.println(ans.GetIntegerSubString(s2));
+		System.out.println(ans.GetIntegerSubString(s3));
+		System.out.println(ans.GetIntegerSubString(s4));
+		System.out.println(ans.GetIntegerSubString(s5));
+		System.out.println(ans.GetIntegerSubString(s6));
+	}
 }
