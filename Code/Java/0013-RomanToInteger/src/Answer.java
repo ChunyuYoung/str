289529667029910ebae103fbf315fb42deaa4975
StringOD <m2719859320@gmail.com>
2019-03-11 09:23:51 +0800

    UpdateJavaCode

--- Code/Java/0013-RomanToInteger/src/Answer.java
+++ Code/Java/0013-RomanToInteger/src/Answer.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
 
 public class Answer {
+	public int GetCharToInteger(char ch){
+		if(ch == 'I')
+			return 1;
+		else if(ch == 'V')
+			return 5;
+		else if(ch == 'X')
+			return 10;
+		else if(ch == 'L')
+			return 50;
+		else if(ch == 'C')
+			return 100;
+		else if(ch == 'D')
+			return 500;
+		else if(ch == 'M')
+			return 1000;
+		return 0;
+	}
 }
--- Code/Java/0013-RomanToInteger/src/Main.java
+++ Code/Java/0013-RomanToInteger/src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
 
 public class Main {
+	public static void main(String args[]){
+		Answer ans = new Answer();
+		System.out.println("I:"+ans.GetCharToInteger('I'));
+		System.out.println("V:"+ans.GetCharToInteger('V'));
+		System.out.println("X:"+ans.GetCharToInteger('X'));
+		System.out.println("L:"+ans.GetCharToInteger('L'));
+		System.out.println("C:"+ans.GetCharToInteger('C'));
+		System.out.println("D:"+ans.GetCharToInteger('D'));
+		System.out.println("M:"+ans.GetCharToInteger('M'));
+	}
 }
