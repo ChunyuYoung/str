e49d9d6b77add2cf5bdfe40833255b9f9d32a414
空白 <m13619199464@163.com>
2019-04-13 13:07:47 +0800

    Update JavaPython Code

---
+++ Code/Java/TestOne/src/Main.java
@@ -0,0 +1,118 @@
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.Scanner;
+
+/*
+前天买的"Java核心技术 卷一",昨天看了一天,了解了一些我当初学习时没有学到的知识
+今天打算实践一下,掌握哪些基本操作,对"Java核心技术 卷一"是一本基础类书籍,这算是我看的第三本基础类书,
+等于是我把Java语言看的第三遍.
+ */
+public class Main {
+    public static void main(String[] args){
+        //数据类型
+        long strL = 400000000L;     //定义长整型
+        int strX = 0xA;             //定义十六进制
+        int strO = 010;             //定义八进制
+        int strB = 0B1000;          //定义二进制
+        int strD = 8_5;             //定义十进制
+        float strF = 3.1415926F;    //定义单精度浮点数
+        double strDouble = Math.E;  //定义双精度浮点数
+        double strd = 1.23456789D;  //定义双精度浮点数
+        System.out.println("输出长整型:" + strL);
+        System.out.println("输出十六进制:" + strX);
+        System.out.println("输出八进制:" + strO);
+        System.out.println("输出二进制:" + strB);
+        System.out.println("输出二进制:" + strD);
+        System.out.println("输出单精度浮点数:" + strF);
+        System.out.println("输出双精度浮点数:" + strDouble);
+        System.out.println("输出双精度浮点数:" + strd);
+
+        //NaN 不是一个数字
+        Scanner sc = new Scanner(System.in);
+        if (Double.isNaN(sc.nextDouble()))
+            System.out.println("不是一个数字");
+        else
+            System.out.println("是一个数字");
+
+        //位运算符
+        int fourthBitFromRight = (8 & 0b1000) /0b1000;
+        System.out.println("位运算:" + fourthBitFromRight);
+
+        //左移,右移运算<<,>>
+        int fourthBitFromL = (16 & (1 << 3)) >> 3;
+        System.out.println("左移和右移运算" + fourthBitFromL);
+
+        //填充高位运算符:>>>
+        int nb = 0b1111;
+        System.out.println("填充高位运算符:" + (nb >>> 0));
+
+        //字符串:子串
+        String name = "张三李四王麻子";
+        System.out.println("字符串截取:" + name.substring(2,4));       //一个汉字在这里占一个
+
+        //字符串拼接
+        String 张三 = "张三";
+        String 李四 = "李四";
+        String 王麻子 = "王麻子";
+        String name_list = String.join("/",张三,李四,王麻子);  //顶一个字符串"/"是定界符,可以使任意字符
+        System.out.println("输出名称列表:" + name_list);
+
+        //检测值类型是否相等
+        if ( 1 == 1)
+            System.out.println("值类型:True");
+
+        //检测引用类型是否相等
+        String sss = "str";
+        if (sss.equals("str"))
+            System.out.println("引用类型:True");
+
+        //检测引用类型是否相等,不区分大小写
+        String ss = "Str";
+        if (ss.equalsIgnoreCase("str") && ss.equalsIgnoreCase("STR"))
+            System.out.println("不区分大小写的引用类型:True");
+
+        //检测字符串是否为空
+        String sVoid = "";
+        if (sVoid.equals("") && sVoid.length() == 0)
+            System.out.println("sVoid为空串");
+
+        //构建字符串
+        StringBuilder str = new StringBuilder();
+        //每次只添加一部分内容
+        str.append("str");
+        str.append("233");
+        System.out.println("构架字符串序列之前:" + str);
+        //构建字符串序列
+        String chStr = str.toString();
+        System.out.println("构建字符串之后:" + chStr);
+
+        //printf标志
+        System.out.printf("printf标志:正数 %d\t负数 %d\n",+233,-233);
+        System.out.printf("printf标志:左对齐-%d\n",233);
+
+        //String 静态方法format 创建格式化字符串
+        String strname = "StringOD";
+        int strage = 21;
+        String message = String.format("Hello Java World,%s.今年%d岁",strname,strage);
+        System.out.printf("使用String的静态方法format构建的字符串:%s\n",message);
+
+        //日期时间 转换符
+        System.out.printf("当前时间: %tc\n",new Date());
+        System.out.printf("当前时间: %BTY\n",new Date());         //这里格式化标志服貌似仅仅支持三个位
+
+        //格式化参数索引
+        System.out.printf("%1$s %2$tB %2$te,%2$tY\n"," Due date:",new Date());
+
+        //读入文件并指定编码
+        try{
+            Scanner in = new Scanner(Paths.get("TempFile.txt"),"UTF-8");
+        }
+        catch (IOException e) {
+            System.out.printf("找不到文件");
+        }
+    }
+}
+
--- Code/Python/HelloPython_4.8/Main.py
+++ Code/Python/HelloPython_4.8/Main.py
@@ -45,7 +45,6 @@ print("修改元素str为StringOD", name_list.pop(0), name_list.insert(0, "Strin
 # 末尾追加
 print("末尾追加:", name_list.append("str233"), "更改后的列表:", name_list)
 
-
 # 函数定义
 '''
 选中函数 会显示黄色小灯泡 点击插入文档注释
@@ -67,7 +66,6 @@ def strInteger(str):
 inte = strInteger(input("请输入一个整数"))
 print(inte, "类型:", type(inte))
 
-
 # len(length 长度) 函数可以统计列表中元素的总数
 name_list_str = ["str1", "str2", "str3", "str1"]
 list_len = len(name_list_str)
@@ -78,5 +76,187 @@ count = name_list_str.count("str1")
 print("str1 出现了 %d 次" % count)
 
 # 从列表中删除第一次出现的数据,如果数据不存在,程序会报错
-name_list_str.remove("str3")    # name_list_str.remove("str6") 会提示元素不在列表中
+name_list_str.remove("str3")  # name_list_str.remove("str6") 会提示元素不在列表中
 print(name_list_str)
+
+# Python 对象引用
+print("123 类型为:", type(123))
+print("123 id为", id(123))
+
+# 通过类对象来创建实例对象
+print("类对象创建实例对象:%d" % (int(123)))
+
+# 变量和对象的引用
+abc_1 = 1
+abc_2 = 2
+# abc_3 = abc_1 + abc_2
+# print("abc_1%d + abc_2%d = %d" % (abc_1, abc_2, abc_3))
+print("%d + %d = %d" % (abc_1, abc_2, abc_1 + abc_2))
+
+print("Abs:", abs(-123))
+
+# 对象的值比较 == 和 引用判断 is
+abc_q = "qq"
+print(1 == 1)
+print(1 is 1)
+print(abc_q is "qq")
+print(abc_q == "qq")
+
+# 链式赋值
+str = str233 = "123"
+print("str:%s\t str233:%s" % (str, str233))
+
+# 复合赋值语句
+num_1 = 1
+num_2 = 2
+# +=
+num_1 += num_2
+print("num_1 += num_2 = %d" % num_1)
+# -=
+num_2 -= num_1
+print("num_2 -= num_1 %d" % num_2)
+# *=
+num_1 *= num_2
+print("num_1 *= num_2 = %d" % num_1)
+# /=
+num_2 /= num_1
+print("num_2 /= num_1 = %d" % num_2)
+# //= 整除
+num_2 //= num_1
+print("num_2 //= num_1 = %d" % num_2)
+# %= 取模
+num_2 %= num_1
+'''
+print("num_2 %= num_1 = %d" % num_2)
+这么写解释器会报错
+原因:%=不是格式控制符,说明解释器认为%=应该是一个格式控制符,但事实上他只是赋值操作符
+解决方法: 表达式写到外部 或者 下面的写法
+'''
+print("num_2 %= num_1", num_2)
+# **= 幂运算
+num_1 **= num_2
+print("num_1 **= num_2 = %d" % num_1)
+# <<= 左移赋值
+num_1 = 2
+num_2 = 4
+num_1 <<= num_2
+print("num_1 <<= num_2 = %d" % num_1)
+# >>= 右移赋值
+num_1 >>= num_2
+print("num_1 >>= num_2 = %d" % num_1)
+# &= 按位于运算
+num_1 &= num_2
+print("num_1 &= num_2 = %d" % num_1)
+# |= 按位异或运算
+num_2 |= num_1
+print("num_2 |= num_1 = %d" % num_2)
+# ^= 按位异或赋值
+num_2 ^= num_1
+print("num_2 ^= num_1 = %d" % num_2)
+
+# del 删除变量
+x = 5
+print("x = %d" % x)
+del x
+'''
+print("删除变量之后: x = %d" % x)
+这么写会报错
+NameError: name 'x' is not defined
+原因:解释器发现x并没有被定义,说明删除变量成功
+'''
+print("x变量 已被删除")
+
+# 系列解包赋值
+num_a, num_b, num_c = 1, 2, 3
+print("num_a = %d\t num_b = %d\t num_c = %d" % (num_a, num_b, num_c))
+num_d, num_e, num_f = (4, 5, 6)
+print("num_d = %d\t num_e = %d\t num_f = %d" % (num_d, num_e, num_f))
+
+# 常量 大写命名
+STR = "str"
+print("STR = %s" % STR)
+
+# 表达式的组成
+import math
+
+# float .48f 貌似是最大支持长度
+print("π值: %.48f" % math.pi)
+
+# Python 运算符及其优先级
+'''
+lambda                  Lambda 表达式
+or                      或
+and                     与
+not                     非
+in, not in              成员测试
+is, is not              同一性测试
+<, <=, >, >=, !=, ==    比较
+|                       按位或
+^                       按位异或
+&                       按位与
+<<, >>                  移位
++, -                    加法与减法
+*, /, %, //             乘法, 除法, 取余, 整数除法
++x, -x                  正负号
+~x                      按位翻转
+**                      指数幂
+x.attribute             属性参考
+x[index]                下标
+x[index:index]          寻址段
+f(arguments...)         函数调用
+(expression,...)        绑定或元组显示
+[expression,...]        列表显示
+{key:datum,...}         字典显示
+'expression,...'        字符串转换
+'''
+
+# Python 语句
+import math  # 导入math模块
+
+r = float(input("请输入圆的半径r: "))
+p = 2 * math.pi * r  # 周长
+s = math.pi * r ** 2  # 面积
+print("圆的周长:", p)
+print("圆的面积:", s)
+
+# 续行符 在要换行的末尾空一格然后输入 续行符
+print("Hello \
+World")
+
+# ; 用于在一行抒写多条语句
+num_g = 1;
+num_h = 2;
+num_i = 3;
+print(num_g, num_h, num_i);
+print();
+print(num_g + num_h + num_i)
+
+
+# 空语句 pass
+def null():
+    pass;
+
+
+# 调用空语句
+null()
+
+
+# 实例对象的创建和调用
+'''
+anObject = 类名(参数列表)
+anObject.对象方法 或 anObject.对象属性
+'''
+
+
+class Person:
+    @staticmethod
+    def sayHello():
+        print("StringOD")
+
+
+# 实例化对象
+p = Person
+# 方法调用
+p.sayHello()
+
+# 2.9 模块和包
